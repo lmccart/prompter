6dc28d6a6b6bb692dcf3a3b53f09fd5b486076d1
Lauren Lee McCarthy <laurenleemccarthy@gmail.com>
2024-08-13 14:08:10 -0700

    lets

--- lines.js
+++ lines.js
@@ -9,8 +9,9 @@ const lines = [
   "Will you allow artificial thoughts into your head tonight?",
   "Throughout the evening, you can look out for us carrying trays of different ear pieces.",
   "If you like, you can select one to insert into your head.",
-  "I want to introduce the performers so you can find us all.",
-  "If you’re feeling lost, feel free to come ask us to help orient you. [NAMES & INTROS]",
+  "But first, I want to introduce the performers so you can find us all.",
+  "[NAMES & INTROS]",
+  "If you’re feeling lost, feel free to come ask us to help orient you.",
   "Please enjoy the tacos and drinks. We’ll bring around some ear pieces for you to try shortly.",
   "And if you don’t get one, don’t worry, there will be several programs.",
   "The ear pieces will be sanitized in between each program.",
@@ -18,33 +19,39 @@ const lines = [
   "There is introvert and extrovert mode. Take whichever you’d like to be.",
 
   "PROGRAM 1",
+
+  "This is the end of program 1. Please join us in the founders room for a program reset.",
     
   "PART 2: FOUNDERS HALL",
 
-  "This is the end of program 1. Please join us in the founders room for a program reset.",
-    "This is the reset. It’s a pause. ",
+  "BREAK 1",
+  
+  "This is the reset. It’s a pause. ",
   "I’m thinking about all the different things I’ve heard so far tonight. What did you hear?",
-  "second pause to reflect.",
-  "Physically speaking, a host is an organism that harbors another guest, a parasite, providing shelter or other resources.",
+  "Pause to reflect.",
+  "Physically speaking, a host is an organism that harbors another guest, a parasite,",
+  "providing shelter or other resources.",
   "The host is usually larger, stronger, or more dominant than its guest.",
-  "We’ve learned well these past years what it is to be sick, to have something else living inside of your body.",
-  "I’m also thinking about the upcoming election, and the impact that may have on our individual decisions about what to host in our bodies. ",
+  "We’ve learned well these past years what it is to be sick, to have something else living inside of you, inside your body.",
+  "I’m also thinking about the election this november, and the impact that will have on our individual decisions about what to host in our bodies.",
   "The host is usually larger, stronger, or more dominant than its guest.",
   "Imagine having a parasite, an entity inside your body, and not being able to make the decision to remove it.",
   "What are we all hosting? What are we holding?",
-  "How do we spent time together right now? How do we feel welcomed?",
+  "How do we spent time together right now? How do we feel welcome?",
   "Ok. So the second program will direct you in a role. You will be able to select from this menu of options.  [SLIDE]",
   "This is not meant to be comprehensive. These are the offerings we have tonight.",
   "You’re also welcome to perform any role you’d like.",
   "We’ll bring around some ear pieces shortly.",
-  "And if you don’t get one, there will be one more program after this. And you can share them.",
+  "And if you don’t get one, there will be one more program after this. And you can share.",
   "Parag K Mital will perform now and host some different ideas, while we reset before the next program.",
   "And guests will be coming and going all evening, please explain to newcomers what’s going on!",  
   
   "PROGRAM 2",
 
   "This is the end of program 2. Please join us in the founders room for the program reset.",
+
   "BREAK 2",
+  
   "This is the reset. It’s one last pause. ",
   "I’m thinking about how I learned to be a good host.",
   "When I was 23 my mom gave me her copy of The Entertaining Survival Guide: A Handbook for the Hesitant Host.",
@@ -60,8 +67,8 @@ const lines = [
   
   "PROGRAM 3",
   
+  "[PLEASE GIVE YOUR EARPIECE TO SOMEONE ELSE THAT COULD USE IT]",
   "This is the end of program 3. We will move to Stern Hall to complete the evening.",
-    
     
   "PART 3: STERN HALL",
   "A host on the network serves resources and data to users that connect to it.",
@@ -71,7 +78,7 @@ const lines = [
   "As the technology flows through us, what we can hold is our sense of presence, our awareness of the people right in front of us.",
   "Thank you performers, parag, sean. [PERFORMERS SPEAK]",
   "Thank you. Stalgia. David. Gabriel Noguez and Tony Noguez and [Photog] for documenting tonight. Wylie and Daniel.",
-  "Kamal, Beata, Taylor, Patrick, Music Center team.",
+  "Kamal, Beata, Taylor, Patrick, Music Center team and crew.",
   "Mashinka, next black bar social in October.",
     
   "Dancing??"
